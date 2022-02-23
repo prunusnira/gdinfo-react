30f88e09dd70e50359601c12f03245de3a981bf8
Prunus Nira <prunusnira@gmail.com>
2022-02-23 19:58:16 +0900

    플레이 횟수 ui 변경

---
+++ src/component/user/playcount/countTable.style.ts
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export const CountWrapper = styled.section`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 10px;
+`;
+
+export const CountJacket = styled.img`
+    width: 75px;
+    height: 75px;
+`;
+
+export const CountPattern = styled.img`
+    width: 90px;
+`;
+export const CountMusic = styled.div``;
+export const CountPlay = styled.div``;
--- src/component/user/playcount/playcount.tsx
+++ src/component/user/playcount/playcount.tsx
@@ -1,36 +1,30 @@
-import React from 'react'
-import { useParams } from 'react-router-dom'
-import { observer } from 'mobx-react'
-import PlayCountPresenter from './playcountPresenter'
-import useUserData from '@/component/common/useUserData'
-import usePlayCount from './usePlayCount'
-import useCountDivCtrl from './useCountDivCtrl'
+import React from "react";
+import { useParams } from "react-router-dom";
+import { observer } from "mobx-react";
+import PlayCountPresenter from "./playcountPresenter";
+import useUserData from "@/component/common/useUserData";
+import usePlayCount from "./usePlayCount";
 
 interface MatchProps {
-    id: string
+    id: string;
 }
 
 const PlayCount = observer(() => {
-    const {id} = useParams<MatchProps>()
-    const [userName, profileLink, titleTower] = useUserData(id)
-    const [plist, glist, dlist, mlist] = usePlayCount(id)
-    const [display0, display1, display2, display3, changeDiv] = useCountDivCtrl()
-    
+    const { id } = useParams<MatchProps>();
+    const [userName, profileLink, titleTower] = useUserData(id);
+    const { plist, glist, dlist, mlist } = usePlayCount(id);
+
     return (
         <PlayCountPresenter
             userName={userName}
             id={id}
             towerTitle={titleTower}
-            display0={display0}
-            display1={display1}
-            display2={display2}
-            display3={display3}
             plist={plist}
             glist={glist}
             dlist={dlist}
             mlist={mlist}
-            changeDiv={changeDiv} />
-    )
-})
+        />
+    );
+});
 
 export default PlayCount;
